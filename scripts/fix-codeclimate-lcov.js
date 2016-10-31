6f25a1d0d06417c333a1c87768f8ae709fad142b
generalov <e.generalov@gmail.com>
2016-10-31 14:46:38 +0300

    coverage (#1)
    add coverage reporting

---
+++ scripts/fix-codeclimate-lcov.js
@@ -0,0 +1,21 @@
+/*eslint-env node */
+/*eslint-disable no-console */
+
+const readline = require('readline');
+const projectRoot = require('path').dirname(__dirname);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+rl.on('line', function (line) {
+  const output = line
+    .replace(new RegExp(`^SF:(.*!)?${escapeRegExp(projectRoot)}.`), 'SF:');
+
+  console.log(output);
+});
